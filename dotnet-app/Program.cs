eb9b493d4fa80f1d918c0c5f3ee4164aad2512a5
Rajkumar_R <--list>
2023-12-21 18:14:59 +0530

    updated dotnet

---
+++ dotnet-app/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string hostName = System.Net.Dns.GetHostName();
+        DateTime currentDateTime = DateTime.Now;
+
+        Console.WriteLine("Hello, World!");
+        Console.WriteLine($"Hostname: {hostName}");
+        Console.WriteLine($"Current Time: {currentDateTime}");
+    }
+}
